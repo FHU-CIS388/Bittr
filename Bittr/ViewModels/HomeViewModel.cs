8e702d9a6fdfbfc3f2eb8e38ae475a8cfb4f800e
arhudg19 <lexi.hudgins@students.fhu.edu>
2019-10-14 13:59:15 -0500

    Update HomeViewModel.cs

--- Bittr/ViewModels/HomeViewModel.cs
+++ Bittr/ViewModels/HomeViewModel.cs
@@ -5,10 +5,11 @@ using Bittr.Models;
 using Xamarin.Forms;
 using Bittr.Services;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Bittr.ViewModels
 {
-    public class HomeViewModel: BaseViewModel
+    public class HomeViewModel : BaseViewModel
     {
         public ObservableCollection<Complaint> Complaints { get; set; }
 
@@ -17,7 +18,8 @@ namespace Bittr.ViewModels
         public IDataStore<Complaint> complaintsDataStore = new MockDataStoreComplaint();
 
         private Complaint newComplaint;
-        public Complaint NewComplaint {
+        public Complaint NewComplaint
+        {
             get
             {
                 return newComplaint;
@@ -39,8 +41,91 @@ namespace Bittr.ViewModels
                 return new Command((c) =>
                 {
                     var complaint = c as Complaint;
-                    complaint.Upvotes++;
-                    complaint.HasUpvoted = true;
+                    if(complaint.HasUpvoted != true)
+                    {
+                        if (complaint.HasDownvoted)
+                        {
+                            complaint.Upvotes++;
+                            complaint.HasUpvoted = true;
+                            complaint.HasDownvoted = false;
+                        }
+                        else
+                        {
+                            complaint.Upvotes++;
+                            complaint.HasUpvoted = true;
+                        }
+                        
+                    }
+                    else
+                    {
+                        complaint.Upvotes--;
+                        complaint.HasUpvoted = false;
+                    }
+                });
+            }
+        }
+
+        public ICommand DownvoteCommand
+        {
+            get
+            {
+                return new Command((c) =>
+                {
+                    var complaint = c as Complaint;
+                    if(complaint.HasDownvoted != true)
+                    {
+                        if (complaint.HasUpvoted)
+                        {
+                            if(complaint.Upvotes == 0)
+                            {
+                                complaint.Upvotes = 0;
+                                complaint.HasDownvoted = true;
+                                complaint.HasUpvoted = false;
+                            }
+                            else
+                            {
+                                complaint.Upvotes--;
+                                complaint.HasDownvoted = true;
+                                complaint.HasUpvoted = false;
+                            }
+                        }
+                        else
+                        {
+                            if (complaint.Upvotes == 0)
+                            {
+                                complaint.HasDownvoted = true;
+                            }
+                            else
+                            {
+                                complaint.Upvotes--;
+                                complaint.HasDownvoted = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        complaint.Upvotes++;
+                        complaint.HasDownvoted = false;
+                    }
+                });
+            }
+        }
+
+        public ICommand FavoriteCommand
+        {
+            get
+            {
+                return new Command((c) =>
+                {
+                    var complaint = c as Complaint;
+                    if (complaint.IsFavorite)
+                    {
+                        complaint.IsFavorite = false;
+                    }
+                    else
+                    {
+                        complaint.IsFavorite = true;
+                    }
                 });
             }
         }
@@ -48,7 +133,7 @@ namespace Bittr.ViewModels
 
         public HomeViewModel()
         {
-            User = new User() { Username="IdontHAVEone", FirstName="Lexi", LastName="Hudgins"};
+            User = new User() { Username = "IdontHAVEone", FirstName = "Lexi", LastName = "Hudgins" };
 
             Complaints = new ObservableCollection<Complaint>();
 
@@ -56,7 +141,7 @@ namespace Bittr.ViewModels
 
             NewComplaint = new Complaint();
 
-            LoadComplaintsCommand = new Command(async () => await LoadComplaints() );
+            LoadComplaintsCommand = new Command(async () => await LoadComplaints());
             PostComplaintCommand = new Command(PostComplaint);
 
             //UpvoteCommand = new Command(async(c) => await UpvoteComplaint(c));
@@ -65,17 +150,35 @@ namespace Bittr.ViewModels
 
         }
 
-        
+
 
         private void PostComplaint()
         {
             NewComplaint.Creator = User;
             NewComplaint.Timestamp = DateTime.Now;
 
+            //text
+            string complain = newComplaint.Text;
+            //regex
+            var regex = new Regex(@"(?<=#)\w+");
+            //matches
+            var matches = regex.Matches(complain);
+            //foreach in match
+            foreach(Match i in matches)
+            {
+                //newtag
+                Tag newTag = new Tag();
+                //hashtag
+                //text in tag
+                newTag.Text = "#" + i.Value;
+                //Complaint to add tag
+                NewComplaint.Tags.Add(newTag);
+            }
+
             //Complaints.Add(NewComplaint);
             Complaints.Insert(0, NewComplaint);
 
-            complaintsDataStore.AddItemAsync(newComplaint);
+            complaintsDataStore.AddItemAsync(NewComplaint);
 
             NewComplaint = new Complaint();
 
@@ -87,7 +190,7 @@ namespace Bittr.ViewModels
 
             Complaints.Clear();
 
-            foreach(var c in results)
+            foreach (var c in results)
             {
                 Complaints.Add(c);
             }
@@ -98,7 +201,6 @@ namespace Bittr.ViewModels
             Console.WriteLine("in upvote");
             var complaint = c as Complaint;
             complaint.Upvotes++;
-
         }*/
 
 
